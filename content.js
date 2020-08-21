f417736cc4ce43c947dcdb55e9faea5abfdfc1d8
Mash <i_mashuk@mail.ru>
2020-08-21 23:16:19 +0300

    after click on the btn get a valid url to the weapon

--- content.js
+++ content.js
@@ -22,7 +22,29 @@ document.querySelector("#reload_button").addEventListener('click',
             item.appendChild(getFromSteam)
             item.addEventListener('contextmenu',
                 function () {
-                console.log(itemsList[item.getAttribute("pos")])
+                    let weaponName = itemsList[item.getAttribute("pos")][0]
+                        .replace(/ /g, "%20")
+                        .replace("|", "%7C")
+
+                    let weaponCvol = () => {
+                        let cvol = itemsList[item.getAttribute("pos")][1]
+                        if (cvol === "FN") {
+                            return "Factory%20New"
+                        }else if (cvol === "MW") {
+                            return "Minimal%20Wear"
+                        }else if (cvol === "WW"){
+                            return "Well-Worn"
+                        }else if (cvol === "FT") {
+                            return "Field-Tested"
+                        }else {
+                            return "Battle-Scarred"
+                        }
+                    }
+                    let weaponFloat = itemsList[item.getAttribute("pos")][2]
+                    let weaponPrice = itemsList[item.getAttribute("pos")][3]
+
+                    console.log(`https://steamcommunity.com/market/listings/730/${weaponName}%28${weaponCvol()}%29`)
+                    console.log(itemsList[item.getAttribute("pos")])
                 })
 
             let price = item.querySelector("div.wrapper__price > div").textContent.trim().slice(3)
